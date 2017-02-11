dccb6959c5ea4320ae1bc34f5db7e8c538dd5285
garycheung13 <garyc102@gmail.com>
2017-02-11 17:47:41 -0500

    problem 1 and 2 answers

--- proj2.py
+++ proj2.py
@@ -1,4 +1,6 @@
 #proj2.py
+from bs4 import BeautifulSoup
+import requests
 
 
 #### Problem 1 ####
@@ -6,15 +8,29 @@ print('\n*********** PROBLEM 1 ***********')
 print('New York Times -- First 10 Story Headings\n')
 
 ### Your Problem 1 solution goes here
+def get_nyt_10(my_url):
+    r = requests.get(my_url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    headline_list = soup.find_all("h2", class_="story-heading")
+    for headline in headline_list[:10]:
+        print(headline.get_text())
 
+get_nyt_10("http://www.nytimes.com")
 
 #### Problem 2 ####
 print('\n*********** PROBLEM 2 ***********')
 print('Michigan Daily -- MOST READ\n')
 
 ### Your Problem 2 solution goes here
+def get_most_read(my_url):
+    r = requests.get(my_url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    most_read_block = soup.find(class_="pane-mostread")
+    article_list = most_read_block.find_all("li")
+    for article in article_list:
+        print(article.get_text())
 
-
+get_most_read("http://michigandaily.com")
 #### Problem 3 ####
 print('\n*********** PROBLEM 3 ***********')
 print("Mark's page -- Alt tags\n")
